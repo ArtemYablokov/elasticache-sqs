2543eabb9ea6a7205e382193ae8a7ce97feba4e1
ArtemYablokov <apple1ya@rambler.ru>
2021-09-10 23:03:23 +0300

    init

---
+++ src/main/java/com/yablokovs/service/SqsSender.java
@@ -0,0 +1,22 @@
+package com.yablokovs.service;
+
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.yablokovs.model.User;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SqsSender {
+
+    private QueueMessagingTemplate queueMessagingTemplate;
+
+
+    public SqsSender(AmazonSQSAsync amazonSQSAsync) {
+        this.queueMessagingTemplate = new QueueMessagingTemplate(amazonSQSAsync);
+    }
+
+    public void send(User user) {
+        System.out.println("Sending user to SQS..." + user.getName());
+        queueMessagingTemplate.convertAndSend("user-cache-details", user);
+    }
+}
